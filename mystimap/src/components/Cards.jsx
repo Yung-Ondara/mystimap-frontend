a9b1bf23509a81a5581e033a45c6aca22ab925a0
daisy <chpdaisy@gmail.com>
2025-04-25 17:18:55 +0300

    done with the function card

---
+++ mystimap/src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Cards.css"
+function Cards(){
+    return(
+        < div className='cards'>
+            <h1>Check Out This EPIC Destinations!</h1>
+        < div className='cards_container'>
+        < div className='cards_wrapper'>
+        </div>
+        </div>
+        </div>
+        )
+    }
+    export default Cards;
