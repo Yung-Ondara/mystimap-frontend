ad90d1ecf2341360028ade9092e6772ed419bd57
hibby <hibaqku7@gmail.com>
2025-04-25 10:44:29 +0300

    Added responsive menu icon and home link with toggle functionality

--- mystimap/src/components/NavBar.jsx
+++ mystimap/src/components/NavBar.jsx
@@ -32,6 +32,16 @@ function NavBar(){
             MyMistiMap
                 <i class='fab fa-typo3' />
                     </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            </ul>
             </div>
         </nav>
         </>
