9d330ea86342d786eb8cb1b4f6068bacefa0ccec
abdulrahman <abdulrahmanibnimohammed@gmail.com>
2025-04-25 12:11:50 +0300

    added function footer paragraphs section

--- mystimap/src/components/Footer.jsx
+++ mystimap/src/components/Footer.jsx
@@ -1,4 +1,17 @@
-import React from 'react';
-import './Footer.css';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Footer.css";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        
+      </section>
+    </div>
+  );
+}
